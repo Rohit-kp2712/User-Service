cbb436af411e2552891052a6835f8d6bd50e7ea2
Rohit-kp2712 <rohit.kp2712@gmail.com>
2024-10-23 17:07:35 -0400

    second commit

--- src/main/java/com/ecommerce/userservice/UserServiceApplication.java
+++ src/main/java/com/ecommerce/userservice/UserServiceApplication.java
@@ -16,6 +16,7 @@ public class UserServiceApplication {
 	void init()
 	{
 		logger.info("Application Started...");
+		logger.info("Application Started3...");
 	}
 	public static void main(String[] args) {
 		logger.info("Application Executed");
